62178d3f2360949ace4f8b5df13574b9f9ac2de3
Nick Chursin <unickq@outlook.com>
2024-09-24 18:18:13 +0300

    upd

--- pure.cjs
+++ pure.cjs
@@ -1,22 +1,31 @@
 const { remote } = require("webdriverio");
 
-const wdOpts = {
-  hostname: "localhost",
-  port: 4723,
-  logLevel: "info",
-  capabilities: {
-    platformName: "iOS",
-    "appium:automationName": "XCUITest",
-    "appium:browserName": "Safari",
-    "appium:deviceName": process.env.ios_model || "iPhone 15",
-    "appium:platformVersion": process.env.ios_version || "17.2",
-  },
+const capabilities = {
+  platformName: "iOS",
+  "appium:automationName": "XCuiTest",
+  "appium:browserName": "Safari",
+  "appium:deviceName": process.env.ios_model || "iPhone 15",
+  "appium:platformVersion": process.env.ios_version || "17.5",
+  "appium:wdaLaunchTimeout": 300_000,
+  "appium:commandTimeouts": 300_000,
+  // "appium:wdaStartupRetries": 5,
+  // "appium:wdaStartupRetryInterval": 30_000,
+  // "appium:wdaConnectionTimeout": 60_000 * 5,
+  "appium:webviewConnectTimeout": 20_000,
+  "appium:webviewConnectRetries": 5,
 };
 
 async function runTest() {
-  const driver = await remote(wdOpts);
+  const driver = await remote({
+    hostname: "localhost",
+    port: 4723,
+    logLevel: "debug",
+    capabilities,
+    connectionRetryTimeout: 300_000,
+    connectionRetryCount: 3,
+  });
   await driver.navigateTo("https://www.google.com");
   console.log(await driver.getTitle());
 }
 
-runTest().catch(console.error);
+runTest();
